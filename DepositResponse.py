7495ab112a65cc75257a8902a38efe9ffdfb1392
Diego Raigoza <draigoza@scu.edu>
2018-06-29 13:23:12 -0700

    Create DepositResponse.py

---
+++ DepositResponse.py
@@ -0,0 +1,35 @@
+from Newtonsoft.Json import *
+
+class DepositResponse(IBankResponse):
+	def get_bank_server(self):
+
+	def set_bank_server(self, value):
+
+	bank_server = property(fget=get_bank_server, fset=set_bank_server)
+
+	def get_time(self):
+
+	def set_time(self, value):
+
+	time = property(fget=get_time, fset=set_time)
+
+	def get_status(self):
+
+	def set_status(self, value):
+
+	status = property(fget=get_status, fset=set_status)
+
+	def get_message(self):
+
+	def set_message(self, value):
+
+	message = property(fget=get_message, fset=set_message)
+
+	def get_receipt(self):
+
+	def set_receipt(self, value):
+
+	receipt = property(fget=get_receipt, fset=set_receipt)
+
+	def __init__(self):
+		pass
